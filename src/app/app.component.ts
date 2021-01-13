bb5f9788cf822a6f6cc8d7f8a6723e48c336557b
Mandarinka450 <nastya-s-01@mail.ru>
2021-01-13 21:43:34 +0300

    server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {
   MyWorkersDatabase,
   MyWorkerType,
 } from './shared/worker.model';
+import { DataService } from './shared/service/data.service';
 
 @Component({
   selector: 'app-root',
@@ -15,23 +16,57 @@ export class AppComponent {
   workers: MyWorker[] = MyWorkersDatabase;
   myWorkerType = MyWorkerType;
 
+  constructor(private DataService: DataService) {}
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  async getData() {
+    try {
+      this.DataService.getWorker().subscribe((data:MyWorker[]) => this.workers=data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   getByType(type: number) {
     return this.workers.filter((worker) => worker.type === type);
   }
 
   onDeleteById(id: number) {
     let index = this.workers.findIndex((worker) => worker.id === id);
+    let id_del:string = String(this.workers[index].id);
     if (index !== -1) {
       this.workers.splice(index, 1);
+      this.DataService.delWorker(id_del).subscribe(()=>this.getData(), (err) => console.log(err));
     }
   }
-
-  onAddWorker(worker) {
-    let id =
-      this.workers.length > 0
-        ? this.workers[this.workers.length - 1].id + 1
-        : 0;
-    worker.id = id;
-    this.workers.push(worker);
+  onEditWorker(editWorker){
+    let index = this.workers.findIndex((worker) => worker.id === editWorker.id);
+    console.log(this.workers)
+    if (index !== -1) {
+      this.workers.splice(index, 1, editWorker);
+      this.DataService.editsWorker(editWorker).subscribe(()=>this.getData(), (err) => console.log(err));
+  }
+}
+  async onAddWorker(worker) {
+    try {
+      let id = this.workers.length > 0 ? this.workers[this.workers.length - 1].id + 1 : 0;
+      worker.id = id;
+      this.workers.push(worker);
+      await this.DataService.postWorker(worker);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      this.getData();
+    }
+    // let id =
+    //   this.workers.length > 0
+    //     ? this.workers[this.workers.length - 1].id + 1
+    //     : 0;
+    // worker.id = id;
+    // this.workers.push(worker);
+    // this.DataService.postWorker(worker).subscribe(()=>this.getData(), (err) => console.log(err));
   }
 }
